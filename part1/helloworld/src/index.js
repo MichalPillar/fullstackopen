285a3c8f76f7dd18a15dfb26ba4300ddf03fea7f
Michal Pillar <michal.pillar@gmail.com>
2020-03-18 13:23:28 -0700

    Updated helloworld app

--- part1/helloworld/src/index.js
+++ part1/helloworld/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
@@ -16,7 +20,7 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="George" age={26 + 10} />
+      <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
     </div>
   )
