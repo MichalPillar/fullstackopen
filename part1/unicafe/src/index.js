6237d3227194d8340a01d0ffac9185362bf7c118
Michal Pillar <michal.pillar@gmail.com>
2020-03-18 23:44:43 -0700

    Exercise 1.11 - Modified statistics data to display in a table

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,21 +3,28 @@ import ReactDOM from 'react-dom';
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Statistic = ({ text, value }) => <div>{text} {value}</div>
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) =>
   // Exercise 1.8 - Statistics already had its own component
   all === 0 ? (
     <div>No feedback given</div>
   ) : (
-      <div>
-        <Statistic text='good' value={good} />
-        <Statistic text='neutral' value={neutral} />
-        <Statistic text='bad' value={bad} />
-        <Statistic text='all' value={all} />
-        <Statistic text='average' value={average} />
-        <Statistic text='positive' value={positive + ' %'} />
-      </div>
+      <table>
+        <tbody>
+          <Statistic text='good' value={good} />
+          <Statistic text='neutral' value={neutral} />
+          <Statistic text='bad' value={bad} />
+          <Statistic text='all' value={all} />
+          <Statistic text='average' value={average} />
+          <Statistic text='positive' value={positive + ' %'} />
+        </tbody>
+      </table>
     );
 
 const App = () => {
