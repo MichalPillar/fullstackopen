31d0e15b8e881b73e4b701ef139c6707d77d5ab7
Michal Pillar <michal.pillar@gmail.com>
2020-03-19 17:44:09 -0700

    Exercise 2.1 - Modified courseinfo app to have components in separate files and to generate parts dynamically

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Course from './components/Course';
+
+// const Total = (props) => (
+//   <p> Number of exercises { props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+// )
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundementals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
