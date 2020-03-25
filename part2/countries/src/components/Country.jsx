ba1106d4a23a5506206aa97fa5771b25f0d906c4
Michal Pillar <michal.pillar@gmail.com>
2020-03-24 19:47:23 -0700

    Exercise 2.13 - Added button to expand/collapse individual country info

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Country = ({ country }) => (
-  <div>
-    <h2>{country.name}</h2>
-    <div>capital {country.capital}</div>
-    <div>population {country.population}</div>
-    <h3>languages</h3>
-    <ul>
-      {
-        country.languages.map(language => (
-          <li key={language.name}>
-            {language.name}
-          </li>
-        ))
-      }
-    </ul>
-    <img src={country.flag} alt={country.name} width="120" />
-  </div>
-)
+const Country = ({ country }) => {
+  const [expand, toggleExpand] = useState(false);
+
+  const handleClick = () => {
+    toggleExpand(!expand)
+  }
+
+  return expand ? (
+    <div>
+      <button onClick={handleClick}>hide</button>
+      <h2>{country.name}</h2>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h3>languages</h3>
+      <ul>
+        {
+          country.languages.map(language => (
+            <li key={language.name}>
+              {language.name}
+            </li>
+          ))
+        }
+      </ul>
+      <img src={country.flag} alt={country.name} width="120" />
+    </div>
+  ) : (
+      <div>
+        {country.name}
+        <button onClick={handleClick}>show</button>
+      </div>
+    )
+
+}
 
 export default Country;
--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import Country from './Country';
 
 const CountryList = ({ countries }) => {
-  if (countries.length > 10) {
-    return countries.length === 250 ? (
+  return countries.length > 10 ? (
+    countries.length === 250 ? (
       <div></div>
     ) : (
         <div>Too many matches, specify another filter</div>
       )
-  } else if (countries.length === 1) {
-    return <Country country={countries[0]} />
-  } else {
-    return countries.map(country => (
-      <div key={country.name}>
-        {country.name}
-      </div>
-    ))
-  }
+  ) : (
+      countries.map(country => (
+        <Country key={country.name} country={country} />
+      ))
+    )
 }
 
 export default CountryList;
